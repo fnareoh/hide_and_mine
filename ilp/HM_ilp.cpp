5ea6dea37e30e631fe538b4de757cb1715b5fa6c
fnareoh <garance.gourdel@laposte.net>
2020-05-26 15:56:49 +0200

    More efficient ilp wrapper

--- ilp/HM_ilp.cpp
+++ ilp/HM_ilp.cpp
@@ -1,6 +1,7 @@
 #include "gurobi_c++.h"
 using namespace std;
 
+#include <chrono>        // chrono
 #include <fstream>       // std::ifstream
 #include <iostream>      // cin, cout
 #include <map>           //map
@@ -9,7 +10,6 @@ using namespace std;
 #include <unordered_set> // unordered_set
 #include <utility>       // pair
 #include <vector>        // vector
-#include <chrono>        // chrono
 
 int k = 0;
 int tau = 0;
@@ -27,7 +27,6 @@ struct Input {
   int nb_critical;
   map<string, int> criticals_index;
   vector<string> list_criticals;
-  vector<vector<vector<int>>> alpha;
   vector<pair<int, int>> forbiden_replacements;
 };
 
@@ -38,32 +37,10 @@ bool is_critical(string kmer, Input &input) {
           input.kmers_frequency[kmer] + k * input.nb_hashmark >= tau);
 }
 
-int count_critical(int i_, int j_, string replaced, Input &input) {
-  for (int it = 0; it <= replaced.size() - k; it++) {
-    std::string kmer = replaced.substr(it, k);
-    if (is_critical(kmer, input)) {
-      if (input.forbiden_patterns.count(kmer) == 1) {
-        input.forbiden_replacements.push_back(make_pair(i_, j_));
-        return 1;
-      } else if (input.criticals_index.count(kmer) == 0) {
-        // First time we see this critical kmer
-        input.criticals_index[kmer] = input.nb_critical;
-        input.list_criticals.push_back(kmer);
-        vector<vector<int>> coeff(input.context_hashmark_index.size(),
-                                  vector<int>(input.alphabet.size(), 0));
-        input.alpha.push_back(coeff);
-        input.nb_critical++;
-      }
-      // account for the kmer added
-      input.alpha[input.criticals_index[kmer]][i_][j_]++;
-    }
-  }
-  return 0;
-}
-
 // Parse the input and get all informtion needed, occurences of kmer, context
 // etc
-void parse_input(string input_file, string forbiden_pattern_file, Input& input) {
+void parse_input(string input_file, string forbiden_pattern_file,
+                 Input &input) {
   unordered_set<char> alphabet;
   ifstream is(input_file); // open file
   char c;
@@ -120,31 +97,13 @@ void parse_input(string input_file, string forbiden_pattern_file, Input& input) 
   }
 }
 
-void compute_replacement(Input& input){
-    // Compute the critical kmer and the number we may add through replacement
-    // (alpha)
-    for (map<pair<string, string>, int>::iterator it =
-             input.context_hashmark_index.begin();
-         it != input.context_hashmark_index.end(); ++it) {
-      auto context = it->first;
-      string replaced;
-      for (int l = 0; l < input.alphabet.size(); l++) {
-        if (input.alphabet[l] == '\0')
-          replaced = context.first + context.second;
-        else
-          replaced = context.first + input.alphabet[l] + context.second;
-        count_critical(it->second, l, replaced, input);
-      }
-    }
-}
-
 void output(vector<vector<char>> replacement, Input &input,
             std::string input_file) {
   int i_hash = 0;
   std::ifstream is(input_file); // open file
   std::ofstream os("output/" +
                    input_file.substr(input_file.find_last_of("/\\") + 1) +
-                   ".output_ILP");
+                   ".output_ilp");
   char c;
   while (is.get(c)) {
     if (c == '\n') {
@@ -175,6 +134,30 @@ void output(vector<vector<char>> replacement, Input &input,
   os.close(); // close file
 }
 
+int count_critical(int i_, int j_, string replaced, Input &input,
+                   vector<GRBLinExpr> &vect_lhs, GRBVar **x) {
+  for (int it = 0; it <= replaced.size() - k; it++) {
+    std::string kmer = replaced.substr(it, k);
+    if (is_critical(kmer, input)) {
+      if (input.forbiden_patterns.count(kmer) == 1) {
+        input.forbiden_replacements.push_back(make_pair(i_, j_));
+        return 1;
+      } else if (input.criticals_index.count(kmer) == 0) {
+        // First time we see this critical kmer
+        input.criticals_index[kmer] = input.nb_critical;
+        input.list_criticals.push_back(kmer);
+        vector<int> coeff(input.alphabet.size(), 0);
+        GRBLinExpr lhs = 0;
+        vect_lhs.push_back(lhs);
+        input.nb_critical++;
+      }
+      // account for the kmer added
+      vect_lhs[input.criticals_index[kmer]] += x[i_][j_];
+    }
+  }
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
   if (argc != 5)
     cout << "You must give 4 arguments: "
@@ -188,9 +171,6 @@ int main(int argc, char *argv[]) {
   parse_input(input_file, forbiden_pattern_file, input);
   cout << "Finished parsing input !" << endl;
 
-
-  compute_replacement(input);
-  cout << "Finished compute_replacement !" << endl;
   std::chrono::steady_clock sc;
   auto start = sc.now();
 
@@ -213,6 +193,23 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < nb_context; i++)
       x[i] = model.addVars(input.alphabet.size(), GRB_INTEGER);
 
+    vector<GRBLinExpr> vect_lhs;
+    // count added critical kmer
+    for (map<pair<string, string>, int>::iterator it =
+             input.context_hashmark_index.begin();
+         it != input.context_hashmark_index.end(); ++it) {
+      auto context = it->first;
+      string replaced;
+      for (int j = 0; j < input.alphabet.size(); j++) {
+        if (input.alphabet[j] == '\0')
+          replaced = context.first + context.second;
+        else
+          replaced = context.first + input.alphabet[j] + context.second;
+        count_critical(it->second, j, replaced, input, vect_lhs, x);
+      }
+    }
+    cout << "Finished counting added critical" << endl;
+
     GRBVar *z = model.addVars(input.nb_critical, GRB_BINARY);
 
     // Create objective
@@ -221,6 +218,13 @@ int main(int argc, char *argv[]) {
       obj += z[l];
     // Set objective: minimize the sum of z
     model.setObjective(obj, GRB_MINIMIZE);
+
+    // limit added kmer or consider it as a ghost
+    for (int l = 0; l < input.nb_critical; l++) {
+      vect_lhs[l] -= z[l] * k * input.nb_hashmark;
+      int e_l = tau - 1 - input.kmers_frequency[input.list_criticals[l]];
+      model.addConstr(vect_lhs[l] <= e_l, "limit occurences or count as ghost");
+    }
 
     // x[i][j] non negatives
     for (int i = 0; i < nb_context; i++) {
@@ -237,19 +241,6 @@ int main(int argc, char *argv[]) {
     }
 
     GRBLinExpr lhs;
-    // limit added kmer or consider it as a ghost
-    for (int l = 0; l < input.nb_critical; l++) {
-      int e_l = tau - 1 - input.kmers_frequency[input.list_criticals[l]];
-      lhs = 0;
-      for (int i = 0; i < nb_context; i++) {
-        for (int j = 0; j < input.alphabet.size(); j++) {
-          lhs += input.alpha[l][i][j] * x[i][j];
-        }
-      }
-      lhs -= z[l] * k * input.nb_hashmark;
-      model.addConstr(lhs <= e_l, "limit occurences or count as ghost");
-    }
-
     int nb_impossible_replacement = 0;
     // All hashmark must have a replacement
     for (int i = 0; i < nb_context; i++) {
@@ -308,13 +299,12 @@ int main(int argc, char *argv[]) {
     cout << "Exception during optimization" << endl;
   }
 
-
   auto end = sc.now();
-  auto time_span = static_cast<chrono::duration<double>> (end - start);
+  auto time_span = static_cast<chrono::duration<double>>(end - start);
   double runtime = time_span.count();
   std::ofstream os_time("output/" +
-                   input_file.substr(input_file.find_last_of("/\\") + 1) +
-                   ".output_ILP_time");
+                        input_file.substr(input_file.find_last_of("/\\") + 1) +
+                        ".output_ilp_time");
   os_time << runtime << std::endl;
   os_time.close();
   return 0;
