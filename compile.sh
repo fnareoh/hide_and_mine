adb611de603a4cb3e926259bdf44d1ed4a471280
fnareoh <garance.gourdel@laposte.net>
2021-05-11 08:53:26 +0200

    Add optimization flag

--- compile.sh
+++ compile.sh
@@ -2,5 +2,6 @@ cd src/ilp
 cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Release
 cmake --build build
 cd ../heuristic
-g++ heuristic.cpp -o heuristic
+g++ -O3 heuristic.cpp -o heuristic
+#g++ -g -Wall -O heuristic.cpp -o heuristic
 cd ../..
