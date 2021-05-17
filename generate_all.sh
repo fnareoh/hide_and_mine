27ebb4e74d083c2289688264af55151b2b71ccb1
fnareoh <garance.gourdel@laposte.net>
2021-05-17 14:40:47 +0200

    Add OLD generation

--- generate_all.sh
+++ generate_all.sh
@@ -8,6 +8,12 @@ generate(){
   ./extra/test.sh $k $tau $file $S
 }
 
+generate_list_input(){
+  for i_input in ${!list_input[@]}
+  do
+    generate $default_k $default_tau ${list_input[$i_input]} $default_y $default_S
+  done
+}
 
 generate_all() {
   for i_k in ${!list_k[@]}
@@ -22,13 +28,15 @@ generate_all() {
   do
     generate $default_k $default_tau $default_input $default_y ${list_S[$i_S]}
   done
-  for i_input in ${!list_input[@]}
-  do
-    generate $default_k $default_tau ${list_input[$i_input]} $default_y $default_S
-  done
+  cat data/results/${name}_k_*_tau_${default_tau}_m_${default_S}.txt.comparison > data/results/${name}.summary
+  cat data/results/${name}_k_${default_k}_tau_*_m_${default_S}.txt.comparison > data/results/${name}_k_${default_k}.summary
+  cat data/results/${name}_k_${default_k}_tau_${default_tau}_m_*.txt.comparison > data/results/${name}_k_${default_k}_tau_${default_tau}.summary
+
 }
 
+generate_SYN(){
 default_input=data/original_data/random20M_char.txt
+name=random20M_char
 list_input=("data/original_data/random5M_char.txt" "data/original_data/random10M_char.txt" "data/original_data/random15M_char.txt" "data/original_data/random20M_char.txt")
 default_k=5
 list_k=(3 4 5 6)
@@ -39,3 +47,22 @@ list_S=(10 100 500 1000)
 default_y=100000
 
 generate_all
+generate_list_input
+}
+
+generate_OLD(){
+default_input=data/original_data/olden_char.txt
+name=olden_char
+list_input=()
+default_k=6
+list_k=(3 4 5 6)
+default_tau=10
+list_tau=(3 5 10 15)
+default_S=120
+list_S=(60 120 240 320)
+default_y=500
+
+generate_all
+}
+
+generate_OLD
