358091c87f01968f59189269ec59f16cd7a4c37a
fnareoh <garance.gourdel@laposte.net>
2021-05-27 17:05:05 +0200

    Fix minor error on nb_hash

--- extra/fasta_to_hasmark_input.py
+++ extra/fasta_to_hasmark_input.py
@@ -6,10 +6,11 @@ in_file = open(name, "r")
 out_file = open(outname, "w")
 parameter_file = open("data/parameters/" + outname.split("/")[-1], "w")
 
+nb_read = 0
 nb_hash = 0
 if name.split(".")[-1] == "txt":
     txt = in_file.read()
-    nb_hash = txt.count("#")
+    nb_read = txt.count("#")
     out_file.write(txt)
 else:
     assert name.split(".")[-1] in ["fasta", "fa"]
@@ -19,11 +20,13 @@ else:
         if not seq:
             break
         if "N" in seq:
-            nb_hash += 1
+            nb_read += 1
+            nb_hash += seq.count("N")
             hash_seq = seq.replace("N", "#")
             out_file.write(hash_seq)
 
 param = [0, 0, 0, nb_hash]
+print("Number of read used:", nb_read)
 for p in param:
     parameter_file.write(str(p) + "\n")
 parameter_file.close()
