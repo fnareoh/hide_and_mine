9ffc3d53a87532f22dcb3999214807f31fb8f64c
fnareoh <garance.gourdel@laposte.net>
2021-10-14 17:50:55 +0200

    Precise Python3 for graph scripts

--- extra/plot/avg_graph.sh
+++ extra/plot/avg_graph.sh
@@ -6,36 +6,36 @@ do
 for FILE in data/results1/${NAME}_char.summary
 do
     F="$(basename -- $FILE)"
-    python extra/plot/plot_avg_bar.py $F k ghosts
-    python extra/plot/plot_avg_bar.py $F k distortion
+    python3 extra/plot/plot_avg_bar.py $F k ghosts
+    python3 extra/plot/plot_avg_bar.py $F k distortion
 done
 
 for FILE in data/results1/${NAME}*char*k_?.summary
 do
    F="$(basename -- $FILE)"
-   python extra/plot/plot_avg_bar.py $F tau ghosts
-   python extra/plot/plot_avg_bar.py $F tau distortion
+   python3 extra/plot/plot_avg_bar.py $F tau ghosts
+   python3 extra/plot/plot_avg_bar.py $F tau distortion
 done
 
 for FILE in data/results1/${NAME}*char*k_??.summary
 do
    F="$(basename -- $FILE)"
-   python extra/plot/plot_avg_bar.py $F tau ghosts
-   python extra/plot/plot_avg_bar.py $F tau distortion
+   python3 extra/plot/plot_avg_bar.py $F tau ghosts
+   python3 extra/plot/plot_avg_bar.py $F tau distortion
 done
 
 for FILE in data/results1/${NAME}*char*k*tau*.summary
 do
    F="$(basename -- $FILE)"
-   python extra/plot/plot_avg_bar.py $F nb_sensitive ghosts
-   python extra/plot/plot_avg_bar.py $F nb_sensitive distortion
+   python3 extra/plot/plot_avg_bar.py $F nb_sensitive ghosts
+   python3 extra/plot/plot_avg_bar.py $F nb_sensitive distortion
 done
 done
 
-python extra/plot/plot_avg_time.py data/results/random20M_char_k_5_tau_10.summary nb_sensitive
-python extra/plot/plot_avg_time.py data/results/random20M_char_k_5.summary tau
-python extra/plot/plot_avg_time.py data/results/random20M_char.summary k
-python extra/plot/plot_avg_time.py data/results/random.summary n
+python3 extra/plot/plot_avg_time.py data/results/random20M_char_k_5_tau_10.summary nb_sensitive
+python3 extra/plot/plot_avg_time.py data/results/random20M_char_k_5.summary tau
+python3 extra/plot/plot_avg_time.py data/results/random20M_char.summary k
+python3 extra/plot/plot_avg_time.py data/results/random.summary n
 
 
 #for i in data/figures/*.svg;do rsvg-convert -f pdf -o ${i%.*}.pdf $i;done
--- extra/plot/graph.sh
+++ extra/plot/graph.sh
@@ -5,36 +5,36 @@ do
 for FILE in data/results/${NAME}_char.summary
 do
     F="$(basename -- $FILE)"
-    python extra/plot/plot_bar.py $FILE k ghosts
-    python extra/plot/plot_bar.py $FILE k distortion
+    python3 extra/plot/plot_bar.py $FILE k ghosts
+    python3 extra/plot/plot_bar.py $FILE k distortion
 done
 
 for FILE in data/results/${NAME}*char*k_?.summary
 do
    F="$(basename -- $FILE)"
-   python extra/plot/plot_bar.py $FILE tau ghosts
-   python extra/plot/plot_bar.py $FILE tau distortion
+   python3 extra/plot/plot_bar.py $FILE tau ghosts
+   python3 extra/plot/plot_bar.py $FILE tau distortion
 done
 
 for FILE in data/results/${NAME}*char*k_??.summary
 do
    F="$(basename -- $FILE)"
-   python extra/plot/plot_bar.py $FILE tau ghosts
-   python extra/plot/plot_bar.py $FILE tau distortion
+   python3 extra/plot/plot_bar.py $FILE tau ghosts
+   python3 extra/plot/plot_bar.py $FILE tau distortion
 done
 
 for FILE in data/results/${NAME}*char*k*tau*.summary
 do
    F="$(basename -- $FILE)"
-   python extra/plot/plot_bar.py $FILE nb_sensitive ghosts
-   python extra/plot/plot_bar.py $FILE nb_sensitive distortion
+   python3 extra/plot/plot_bar.py $FILE nb_sensitive ghosts
+   python3 extra/plot/plot_bar.py $FILE nb_sensitive distortion
 done
 done
 
-python extra/plot/plot_time.py data/results/random20M_char_k_5_tau_10.summary nb_sensitive
-python extra/plot/plot_time.py data/results/random20M_char_k_5.summary tau
-python extra/plot/plot_time.py data/results/random20M_char.summary k
-python extra/plot/plot_time.py data/results/random.summary n
+python3 extra/plot/plot_time.py data/results/random20M_char_k_5_tau_10.summary nb_sensitive
+python3 extra/plot/plot_time.py data/results/random20M_char_k_5.summary tau
+python3 extra/plot/plot_time.py data/results/random20M_char.summary k
+python3 extra/plot/plot_time.py data/results/random.summary n
 
 
 #for i in data/figures/*.svg;do rsvg-convert -f pdf -o ${i%.*}.pdf $i;done
